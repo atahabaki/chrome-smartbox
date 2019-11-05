f260399e30c3f639273b0703fa6d5edeed9510cb
A. Taha Baki <a.taha.baki@outlook.com>
2019-11-05 22:22:00 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# chrome-multi-searchbox
+
+Just type "@" in URL bar. Then type the search engine you wanna search e.g. 'youtube', 'google', 'bing' etc. Then type the search string.
+
+Use it well...
+
+## Available Search Engines & Keywords
+
+| Platform    | Keyword     |
+|-------------|-------------|
+| YouTube     | youtube     |
+| Google      | google      |
+| Bing        | bing        |
+| DuckDuckGo  | duckduckgo  |
+| SesliSozluk | seslisozluk |
+| Yahoo       | yahoo       |
+| TorrentOyun | torroyun    |
+| Yandex      | yandex      |
+| AllItEBooks | allitebooks |
---
+++ app.js
@@ -0,0 +1,55 @@
+const x = {
+    "youtube": "https://www.youtube.com/results?search_query=",
+    "google": "https://www.google.com/search?q=",
+    "bing": "https://www.bing.com/search?q=",
+    "duckduckgo": "https://duckduckgo.com/?q=",
+    "seslisozluk": "https://www.seslisozluk.net/",
+    "yahoo": "https://search.yahoo.com/?q=",
+    "torroyun": "https://www.torrentoyunindir.com/?s=",
+    "yandex": "https://yandex.com.tr/search/?text=",
+    "allitebooks": "http://www.allitebooks.org/?s="
+}
+function createURL(text, query) {
+    if (x[text] != undefined) {
+        return `${x[text]}${query}`;
+    }
+    else {
+        return `${text}${query}`;
+    }
+}
+chrome.omnibox.onInputEntered.addListener((text, disposition) => {
+    disposition = "currentTab";
+    var arr, site, query;
+    arr = text.split(' ');
+    if (arr.length > 1) {
+        site = arr[0].toLowerCase().trim();
+        arr.shift();
+        query = arr.join(" ").trim();
+        chrome.tabs.update({
+            url: createURL(site, query)
+        });
+    }
+});
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+    var arr, site, query;
+    arr = text.split(' ');
+    if (arr.length > 1) {
+        site = arr[0].toLowerCase().trim();
+        arr.shift();
+        query = arr.join(" ").trim();
+
+        if (x[site] != undefined) {
+            suggest([
+                { content: `${x[site]}${query}`, description: `Search on ${site} for ${query}` }
+            ]);
+        }
+        else {
+            suggest([
+                { content: `http://${site} ${query}`, description: `Search on ${site} for ${query}` },
+                { content: `http://${site}.com/ ${query}`, description: `Search on ${site}.com/ for ${query}` },
+                { content: `http://${site}.net/ ${query}`, description: `Search on ${site}.net/ for ${query}` },
+                { content: `http://${site}.org/ ${query}`, description: `Search on ${site}.org/ for ${query}` },
+            ]);
+        }
+    }
+});
