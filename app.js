4d4a0796fa4555f2a258f2a00dba4f5847298b82
A. Taha Baki <a.taha.baki@outlook.com>
2020-03-27 00:47:46 +0300

    v 2.0

--- app.js
+++ app.js
@@ -1,56 +1,132 @@
-const x = {
-    "youtube": "https://www.youtube.com/results?search_query=",
-    "google": "https://www.google.com/search?q=",
-    "bing": "https://www.bing.com/search?q=",
-    "duckduckgo": "https://duckduckgo.com/?q=",
-    "seslisozluk": "https://www.seslisozluk.net/",
-    "yahoo": "https://search.yahoo.com/?q=",
-    "torroyun": "https://www.torrentoyunindir.com/?s=",
-    "yandex": "https://yandex.com.tr/search/?text=",
-    "allitebooks": "http://www.allitebooks.org/?s=",
-    "iconfinder":"https://www.iconfinder.com/search/?q="
-}
-function createURL(text, query) {
-    if (x[text] != undefined) {
-        return `${x[text]}${query}`;
+const sites = [
+    {
+        "name":"ytb",
+        "desc":"YouTube",
+        "url":"https://www.youtube.com/results?search_query="
+    },
+    {
+        "name":"ggl",
+        "desc":"Google",
+        "url":"https://www.google.com/search?q="
+    },
+    {
+        "name":"bng",
+        "desc":"Bing",
+        "url":"https://www.bing.com/search?q="
+    },
+    {
+        "name":"dckgo",
+        "desc":"DuckDuckGo",
+        "url":"https://duckduckgo.com/?q="
+    },
+    {
+        "name":"sszlk",
+        "desc":"SesliSözlük",
+        "url":"https://seslisozluk.net/"
+    },
+    {
+        "name":"yaho",
+        "desc":"Yahoo",
+        "url":"https://search.yahoo.com/?q="
+    },
+    {
+        "name":"trryn",
+        "desc":"Torrent Oyun İndir",
+        "url":"https://www.torrentoyunindir.com/?s="
+    },
+    {
+        "name":"ydex",
+        "desc":"Yandex",
+        "url":"https://yandex.com/search/?text="
+    },
+    {
+        "name":"itebks",
+        "desc":"All IT e-Books",
+        "url":"http://allitebooks.org/?s="
+    },
+    {
+        "name":"iconf",
+        "desc":"IconFinder",
+        "url":"https://www.iconfinder.com/search/?q="
+    },
+    {
+        "name":"rddt",
+        "desc":"Reddit",
+        "url":"https://www.reddit.com/search/?q="
     }
-    else {
-        return `${text}${query}`;
+];
+
+function createURL(site,query) {
+    for (var i = 0; i < sites.length; i++) {
+        if ( sites[i].name == site ) {
+            return `${sites[i].url}${query}`
+        }
     }
 }
+
 chrome.omnibox.onInputEntered.addListener((text, disposition) => {
     disposition = "currentTab";
     var arr, site, query;
     arr = text.split(' ');
-    if (arr.length > 1) {
+    if ( arr.length > 1 ) {
         site = arr[0].toLowerCase().trim();
         arr.shift();
         query = arr.join(" ").trim();
+        var createdurl = createURL(site,query);
         chrome.tabs.update({
-            url: createURL(site, query)
+            url: createdurl
         });
     }
 });
-chrome.omnibox.onInputChanged.addListener((text, suggest) => {
-    var arr, site, query;
-    arr = text.split(' ');
-    if (arr.length > 1) {
-        site = arr[0].toLowerCase().trim();
-        arr.shift();
-        query = arr.join(" ").trim();
 
-        if (x[site] != undefined) {
-            suggest([
-                { content: `${x[site]}${query}`, description: `Search on ${site} for ${query}` }
-            ]);
-        }
-        else {
-            suggest([
-                { content: `http://${site} ${query}`, description: `Search on ${site} for ${query}` },
-                { content: `http://${site}.com/ ${query}`, description: `Search on ${site}.com/ for ${query}` },
-                { content: `http://${site}.net/ ${query}`, description: `Search on ${site}.net/ for ${query}` },
-                { content: `http://${site}.org/ ${query}`, description: `Search on ${site}.org/ for ${query}` },
-            ]);
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+    if ( text.trim().length == 0 ) {
+        sites.forEach(e => {
+            suggest(
+                [
+                    {
+                        content: `${e.url}`, description: `Search on ${e.name} (${e.desc})`
+                    }
+                ]
+            );
+        });
+    }
+    else {
+        var arr, site, query;
+        arr = text.split(' ');
+        if ( arr.length > 1) {
+            site = arr[0].toLowerCase().trim();
+            arr.shift();
+            query = arr.join(" ").trim();
+
+            sites.forEach(e => {
+                if ( e.name === site ) {
+                    suggest(
+                        [
+                            {
+                                content: `${e.url}${query}`, description: `Search for ${query} on ${e.desc}`
+                            }
+                        ]
+                    );
+                }
+                else if ( e.name.includes(site) ) {
+                    suggest(
+                        [
+                            {
+                                content: `${e.url}${query}`, description: `Search for ${query} on ${e.desc}`
+                            }
+                        ]
+                    );
+                }
+                else {
+                    suggest([
+                        { content: `${site}/${query}`, description: `Search on ${site} for ${query}` },
+                        { content: `${site}.com/${query}`, description: `Search on ${site}.com/ for ${query}` },
+                        { content: `${site}.net/${query}`, description: `Search on ${site}.net/ for ${query}` },
+                        { content: `${site}.org/${query}`, description: `Search on ${site}.org/ for ${query}` },
+                    ]);
+                }
+            });
         }
     }
 });
