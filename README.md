52eaedc4ffe5e3672f4ea93361b717fa831584d3
A. Taha Baki <a.taha.baki@outlook.com>
2020-06-27 01:37:31 +0300

    general fixes

--- README.md
+++ README.md
@@ -1,32 +1,27 @@
 ![SmartBox](images/logo-design.svg)
-# chrome-smartbox
 
-`smartbox v4.1`
+# smartbox
 
-Searching would not be much simpler. Just type "@" in URL bar. Then type the search engine you wanna search e.g. 'ytb', 'ggl', 'bng' etc. Then type the search string.
+`smartbox v4.6`
 
-Use it well...
+An unmatched searching tool for web browsers, search for
+anything you want, search on anywhere you want.
 
 ## Available Search Engines & Keywords
 
-| Platform     | Keyword   |
-|--------------|-----------|
-| YouTube      | ytb       |
-| Google       | ggl       |
-| Android Docs | dvand     |
-| Bing         | bng       |
-| DuckDuckGo   | dckgo     |
-| SesliSozluk  | sszlk     |
-| Yahoo        | yaho      |
-| Torrent O... | trryn     |
-| Yandex       | ydex      |
-| All IT e-b...| itebx     |
-| Iconfinder   | iconf     |
-| Reddit       | rddt      |
-| TorrentMafya | trrmf     |
-| GitHub       | gthb      |
+| Platform           | Keyword   |
+|:-------------------|:---------:|
+| YouTube            | ytb       |
+| Google             | ggl       |
+| Android Developers | adox      |
+| Bing               | bng       |
+| DuckDuckGo         | dckgo     |
+| Yahoo              | yaho      |
+| Iconfinder         | iconf     |
+| Reddit             | rddt      |
+| GitHub             | gthb      |
 
-## Screenshot
+## Screenshots
 
 ![Searching on YouTube about chrome-smartbox](media/chrome-smartbox-search-focused.png)
 ![Listing all search engines by comma seperated list](media/chrome-smartbox-listing-comma-focused.png)
@@ -34,7 +29,7 @@ Use it well...
 
 ## Usage
 
-### JSON data...
+### JSON data
 
 ```json
 [
@@ -50,18 +45,27 @@ Use it well...
 
 Simple to search on whatever you want.
 
-```
+```text
 @ <keyword> <search_query>
 ```
 
 keyword: is the short version of the web site's name.
-search_query: search query, nothing special. Just I need to mention here it'll be encoded.
+search_query: search query, nothing special. Just I need to mention here
+it'll be encoded.
+
+### Change Default Search Engine
+
+Simple to change default search engine.
+
+```text
+@ !<keyword>
+```
 
 ### Adding a search engine
 
 Adding search engine to smartbox is simple. Please just type:
 
-```
+```text
 @ +<keyword> <site_name> <url>
 ```
 
@@ -73,7 +77,7 @@ url: can be similar to this one: "https://example.com/search?q="
 
 Removing a search engine from database as simple as adding one. Please just type:
 
-```
+```text
 @ -<keyword>
 ```
 
@@ -83,15 +87,16 @@ keyword: is the short-name of the web site.
 
 There're 2 options you can try to list all of them. The first one is:
 
-```
+```text
 @ .
 ```
 
 You can list just specific amount of them by typing "?".
 
-The other one is my favorite. You can quickly review what's available in comma seperated form. Try:
+The other one is my favorite. You can quickly review
+what's available in comma seperated form. Try:
 
-```
+```text
 @ ...
 ```
 
@@ -100,21 +105,27 @@ Which one is better?
 ### Importing, Exporting and Adding sites
 
 Importing, easily pasting prepared JSON array of search engines...
-```
+
+```text
 @ <= <json_array>
 ```
 
 Exporting, easly while on omnibox. :D
-```
+
+```text
 @ >=
 ```
 
 Adding array of search engines, it's now easier. :D
-```
+
+```text
 @ +<= <json_array>
+```
 
 ## ChangeLog
 
+* Improvement, general fixes and etc.
+* Ability to make default search engine...
 * Storing changes locally. (Finally)
 * Importing, Exporting and Adding sites (Experimental, but yes :D)
 * General improvements and fixes.
--- app.js
+++ app.js
@@ -1,241 +1,335 @@
 var sites = [
-    {
-        "name":"ytb",
-        "desc":"YouTube",
-        "url":"https://www.youtube.com/results?search_query={%query%}"
-    },
-    {
-        "name":"ggl",
-        "desc":"Google",
-        "url":"https://www.google.com/search?q={%query%}"
-    },
-    {
-        "name":"dvand",
-        "desc": "Android Devs",
-        "url": "https://developer.android.com/s/results?q={%query%}"
-    },
-    {
-        "name":"bng",
-        "desc":"Bing",
-        "url":"https://www.bing.com/search?q={%query%}"
-    },
-    {
-        "name":"dckgo",
-        "desc":"DuckDuckGo",
-        "url":"https://duckduckgo.com/?q={%query%}"
-    },
-    {
-        "name":"sszlk",
-        "desc":"SesliSözlük",
-        "url":"https://seslisozluk.net/{%query%}"
-    },
-    {
-        "name":"yaho",
-        "desc":"Yahoo",
-        "url":"https://search.yahoo.com/?q={%query%}"
-    },
-    {
-        "name":"trryn",
-        "desc":"Torrent Oyun İndir",
-        "url":"https://www.torrentoyunindir.com/?s={%query%}"
-    },
-    {
-        "name":"ydex",
-        "desc":"Yandex",
-        "url":"https://yandex.com/search/?text={%query%}"
-    },
-    {
-        "name":"itebx",
-        "desc":"All IT e-Books",
-        "url":"http://allitebooks.org/?s={%query%}"
-    },
-    {
-        "name":"iconf",
-        "desc":"IconFinder",
-        "url":"https://www.iconfinder.com/search/?q={%query%}"
-    },
-    {
-        "name":"rddt",
-        "desc":"Reddit",
-        "url":"https://www.reddit.com/search/?q={%query%}"
-    },
-    {
-        "name":"trrmfy",
-        "desc":"TorrentMafya",
-        "url":"https://www.torrentmafya.net/?s={%query%}"
-    },
-    {
-        "name":"gthb",
-        "desc":"GitHub",
-        "url":"https://github.com/search?q={%query%}"
-    }
+	{
+		"name":"ytb",
+		"desc":"YouTube",
+		"url":"https://www.youtube.com/results?search_query={%query%}"
+	},
+	{
+		"name":"ggl",
+		"desc":"Google",
+		"url":"https://www.google.com/search?q={%query%}"
+	},
+	{
+		"name":"adox",
+		"desc": "Android Developers",
+		"url": "https://developer.android.com/s/results?q={%query%}"
+	},
+	{
+		"name":"bng",
+		"desc":"Bing",
+		"url":"https://www.bing.com/search?q={%query%}"
+	},
+	{
+		"name":"dckgo",
+		"desc":"DuckDuckGo",
+		"url":"https://duckduckgo.com/?q={%query%}"
+	},
+	{
+		"name":"yaho",
+		"desc":"Yahoo",
+		"url":"https://search.yahoo.com/?q={%query%}"
+	},
+	{
+		"name":"iconf",
+		"desc":"IconFinder",
+		"url":"https://www.iconfinder.com/search/?q={%query%}"
+	},
+	{
+		"name":"rddt",
+		"desc":"Reddit",
+		"url":"https://www.reddit.com/search/?q={%query%}"
+	},
+	{
+		"name":"gthb",
+		"desc":"GitHub",
+		"url":"https://github.com/search?q={%query%}"
+	}
 ];
-
+var default_one = sites.filter(e => e.name == "ggl")[0];
 
 function save() {
-    chrome.storage.local.set({"ssearch_sites":sites}, () => {
-        console.log("Saved!",sites);
-        sites = sites;
-    });
+	chrome.storage.local.set({"ssearch_sites":sites, "default_one": default_one}, () => {
+		console.log("Saved!",sites,default_one);
+	});
 }
 
 function load() {
-    chrome.storage.local.get('ssearch_sites', (res) => {
-        console.log("Got!",res.ssearch_sites);
-        sites = res.ssearch_sites;
-    });
+	console.log(chrome.i18n.getUILanguage())
+	chrome.storage.local.get(['ssearch_sites','default_one'], (res) => {
+		console.log("Got!",res.ssearch_sites, res.default_one);
+		sites = res.ssearch_sites;
+		default_one = res.default_one;
+	});
+}
+
+function changeDef(site) {
+	let _default_one = sites.filter(e => e.name == site)[0];
+	if (_default_one != undefined) {
+		default_one = _default_one;
+		save();
+	}
+	else return
 }
 
 chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason == "install") {
-        console.log("First install");
-        save();
-        load();
-    }
+	if (details.reason == "install") {
+		console.log("First install");
+		save();
+		load();
+	}
 });
 
 load();
 
 function createURL(site,query) {
-    for (var i = 0; i < sites.length; i++) {
-        if ( sites[i].name == site ) {
-            return `${sites[i].url.replace("{%query%}",query)}`
-        }
-    }
+	for (var i = 0; i < sites.length; i++) {
+		if ( sites[i].name == site ) {
+			return `${sites[i].url.replace("{%query%}",query)}`
+		}
+	}
 }
 
-chrome.omnibox.onInputEntered.addListener((text, disposition) => {
-    disposition = "currentTab";
-    var arr, site, query;
-    arr = text.split(' ');
-    let operation = arr[0];
-    console.log(sites);
-    if (operation === "+" ) {
-        let url = arr[arr.length - 1];
-        arr.pop();
-        arr.shift();
-        let name = arr.join(' ');
-        sites.push(
-            {
-                "name":`${operation.substring(1)}`,
-                "desc":name,
-                "url":url
-            }
-        );
-        save();
-        load();
-    }
-    else if (operation === "-") {
-        for (var i = 0; i < sites.length; i++) {
-            if ( sites[i].name == operation.substring(1) ) {
-                sites.splice(i,1);
-            }
-        }
-        save();
-        load();
-    }
-    else if (operation === "<=" ) {
-        let temp = text.substring(text.indexOf("["));
-        let temp_sites = JSON.parse(temp);
-        sites = temp_sites;
-        save();
-        load();
-    }
-    else if (operation === "+<=" ) {
-        let temp = text.substring(text.indexOf("["));
-        let temp_sites = JSON.parse(temp);
-        for (var i = 0; i < temp_sites.length; i++) {
-            sites.push(temp_sites[i]);
-        }
-        save();
-        load();
-    }
-    else {
-        if ( arr.length > 1 ) {
-            site = arr[0].toLowerCase().trim();
-            arr.shift();
-            query = arr.join(" ").trim();
-            var createdurl = createURL(site,query);
-            chrome.tabs.update({
-                url: createdurl
-            });
-        }
-    }
+function navigate(url) {
+	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+		chrome.tabs.update(tabs[0].id, {url: url});
+	});
+}
+
+let add = /^(\+<=)[\s\t\v]*(\[.*\])/;
+let imp = /^(<=)[\s\t\v]*(\[.*\])/;
+let sng_add = /^(\+)([\w\s\d]+)[\s\t\v]+([\w\d\s]+)[\s\t\v]+(.*)/;
+let sng_rm = /^(\-)([\w\s\d]+)/;
+let def_one = /^(\!)([\w\s\d]+)/;
+let search = /^([\w\d]+)[\s\t\v]+(.*)/;
+
+chrome.omnibox.onInputEntered.addListener((text) => {
+	text = text.trim();
+	console.log(sites);
+
+	if (add.test(text)) {
+		console.log(`It is +<=`);
+		let _res = text.match(add);
+		if ( _res.length == 3 ) {
+			let _sites = JSON.parse(_res[2]);
+			_sites.forEach(e => sites.push(e));
+			save();
+		}
+	}
+	else if (imp.test(text)) {
+		console.log(`It is <=`);
+		let _res = text.match(imp);
+		if ( _res.length == 3 ) {
+			sites = JSON.parse(_res[2]);
+			save();
+		}
+	}
+	else if (sng_add.test(text)) {
+		console.log(`It is +`);
+		let site = {
+			name: null,
+			desc: null,
+			url: null
+		};
+		let _res = text.match(sng_add);
+		if ( _res.length == 5 ) {
+			site.name = _res[2];
+			site.desc = _res[3];
+			site.url = _res[4];
+			sites.push(site);
+			save();
+		}
+	}
+	else if (sng_rm.test(text)) {
+		console.log(`It is -`);
+		let _res = text.match(sng_rm);
+		if (_res.length == 3) {
+			let site = _res[2];
+			sites = sites.filter(e => e.name != site);
+			save();
+		}
+	}
+	else if (def_one.test(text)) {
+		console.log(`It is !`);
+		let _res = text.match(def_one);
+		if (_res.length == 3) {
+			let site = _res[2];
+			changeDef(site);
+		}
+	}
+	else if (search.test(text)) {
+		let _res = text.match(search);
+		if (_res.length == 3) {
+			let site = _res[1];
+			let query = _res[2];
+			let url = createURL(site, query);
+			if (url != undefined) {
+				navigate(url);
+				suggest(suggestions);
+			}
+			else {
+				let _url = createURL(default_one.name, text);
+				console.log(_url);
+				navigate(_url);
+			}
+		}
+	}
 });
 
+const getStrChange = (site, query) =>	 chrome.i18n.getMessage("search").replace("%site", site).replace("%query", query);
+
+
 function rawSuggest(site, query) {
-    return [
-        { content: `${site}/${query}`, description: `Search on ${site} for ${query}` },
-        { content: `${site}.com/${query}`, description: `Search on ${site}.com/ for ${query}` },
-        { content: `${site}.net/${query}`, description: `Search on ${site}.net/ for ${query}` },
-        { content: `${site}.org/${query}`, description: `Search on ${site}.org/ for ${query}` },
-    ];
+	return [
+		{ content: `${site}/${query}`, description: getStrChange(site,query) },
+		{ content: `${site}.com/${query}`, description: getStrChange(`${site}.com`,query) },
+		{ content: `${site}.net/${query}`, description: getStrChange(`${site}.net`,query) },
+		{ content: `${site}.org/${query}`, description: getStrChange(`${site}.org`,query) },
+	];
 }
 
 chrome.omnibox.onInputChanged.addListener((text, suggest) => {
-    text = text.trim();
-    if ( text == "." ) {
-        let suggestions = [];
-        sites.forEach(e => {
-            let temp = {
-                content: `${e.url}`, description: `Search on ${e.name} (${e.desc})`
-            };
-            suggestions.push(temp);
-        });
-        suggest(suggestions);
-    }
-    else if ( text == "..." ) {
-        let content = "";
-        for (let i=0; i < sites.length; i++) {
-            if ( i == sites.length - 1) {
-                content += sites[i].name;
-            }
-            else content += `${sites[i].name}, `;
-        }
-        suggest(
-            [
-                {
-                    content: content, description: content
-                }
-            ]
-        );
-    }
-    else if ( text == "=>" ) {
-        load();
-        console.log(sites);
-        suggest(
-            [
-                {
-                    content: JSON.stringify(sites), description: "Export to JSON"
-                }
-            ]
-        );
-    }
-    else {
-        var arr, site, query;
-        arr = text.split(' ');
-        if ( arr.length > 1) {
-            site = arr[0].toLowerCase().trim();
-            arr.shift();
-            query = arr.join(" ").trim();
+	console.log(sites);
+	text=text.trim();
 
-            let suggestions = [];
-
-            sites.forEach(e => {
-                let temp = {
-                    content: "",
-                    description: ""
-                };
-                if (e.name === site || e.name.includes(site)) {
-                    temp.content = `${e.url.replace('{%query%}',query)}`;
-                    temp.description = `Search for ${query} on ${e.desc}`;
-                    suggestions.push(temp);
-                }
-                else {
-                    rawSuggest(site,query).forEach(e=>suggestions.push(e));
-                }
-            });
-            suggest(suggestions);
-        }
-    }
+	if (add.test(text)) {
+		console.log(`It is +<=`);
+		let _sites = [];
+		let _res = text.match(add);
+		if ( _res.length == 3 ) {
+			_sites = _res[2]
+		}
+		suggest([
+			{
+				content: _sites, description: chrome.i18n.getMessage("add_more")
+			}
+		]);
+	}
+	else if (imp.test(text)) {
+		console.log(`It is <=`);
+		let _sites = [];
+		let _res = text.match(imp);
+		if ( _res.length == 3 ) {
+			_sites = _res[2]
+			suggest([
+				{
+					content: _sites, description: chrome.i18n.getMessage("import")
+				}
+			]);
+		}
+	}
+	else if (sng_add.test(text)) {
+		console.log(`It is +`);
+		let site = {
+			name: null,
+			desc: null,
+			url: null
+		};
+		let _res = text.match(sng_add);
+		if ( _res.length == 5 ) {
+			site.name = _res[2];
+			site.desc = _res[3];
+			site.url = _res[4];
+			suggest([
+				{
+					content: _res.length === 5 ? JSON.stringify(site) : undefined,
+					description: chrome.i18n.getMessage("add_one").replace("%name",site.name)
+					.replace("%desc",site.desc)
+					.replace("%url",site.url)
+				}
+			]);
+		}
+	}
+	else if (sng_rm.test(text)) {
+		console.log(`It is -`);
+		let _res = text.match(sng_rm);
+		if (_res.length == 3) {
+			let site = _res[2];
+			let rm_sites = sites.filter(e => e.name != site); //Removed state
+			let entry = sites.filter(e => e.name == site)[0]; //Get Result
+			suggest([
+				{
+					content: rm_sites.length != 0 ? JSON.stringify(rm_sites) : undefined,
+					description: chrome.i18n.getMessage("rmentry").replace("%entry", `${entry.name} (${entry.desc})`)
+				}
+			]);
+		}
+	}
+	else if (def_one.test(text)) {
+		console.log(`It is !`);
+		let _res = text.match(def_one);
+		if (_res.length == 3) {
+			let site = _res[2];
+			_def_one = sites.filter(e => e.name == site);
+			_def_one.length == 1 && suggest([
+				{
+					content: JSON.stringify(_def_one),
+					description: chrome.i18n.getMessage("setdef").replace("%entry", `${_def_one[0].name} (${_def_one[0].desc})`)
+				}
+			]);
+		}
+	}
+	else if ( text == "." ) {
+		let suggestions = [];
+		sites.forEach(e => {
+			let temp = {
+				content: `${e.url}`, description: chrome.i18n.getMessage("list_engines")
+				.replace("%s",e.name)
+				.replace("%n",e.desc)
+			};
+			suggestions.push(temp);
+		});
+		suggest(suggestions);
+	}
+	else if ( text == "..." ) {
+		let content = "";
+		for (let i=0; i < sites.length; i++) {
+			if ( i == sites.length - 1) {
+				content += sites[i].name;
+			}
+			else content += `${sites[i].name}, `;
+		}
+		suggest(
+			[
+				{
+					content: content, description: content
+				}
+			]
+		);
+	}
+	else if ( text == "=>" ) {
+		load();
+		console.log(sites);
+		suggest(
+			[
+				{
+					content: JSON.stringify(sites), description: chrome.i18n.getMessage("export_json")
+				}
+			]
+		);
+	}
+	else if (search.test(text)) {
+		let suggestions = [];
+		let _res = text.match(search);
+		if (_res.length == 3) {
+			let site = _res[1];
+			let query = _res[2];
+			sites.forEach(e => {
+				let _temp = {
+					content: '',
+					description: ''
+				};
+				if (e.name == site || e.name.includes(site)) {
+					_temp.content = `${e.url.replace('{%query%}',query)}`;
+					_temp.description = chrome.i18n.getMessage('search')
+						.replace("%site", e.desc)
+						.replace("%query", query)
+					suggestions.push(_temp);
+				}
+				else {
+					rawSuggest(site, query).forEach(e=>suggestions.push(e));
+				}
+			});
+		}
+		suggestions.length != 0 && suggest(suggestions);
+	}
 });
