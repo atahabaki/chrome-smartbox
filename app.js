7489d23e78ebfcb716cbb8922cd147d4f9cf4b94
A. Taha Baki <a.taha.baki@outlook.com>
2020-12-08 23:43:03 +0300

    searchOnDefault1 fixed, manifest stuff...

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const info = msg => console.log(msg);
-const error = msg => console.error(msg);
 const is_object = data => typeof data === "object";
 
 class Box {
-	constructor(name,description,url) {
+	constructor(name, description, url) {
 		this.name = name;
 		this.desc = description;
 		this.url = url;
@@ -32,29 +30,35 @@ class Box {
 		}
 		else return false;
 	}
-		
+
 	fromJSON(data) {
 		if (this.validate(data)) {
-			info("Valid, good!");
+			console.log("Valid, good!");
 		}
 		return this;
 	}
 
 	toJSON() {
 		return {
-			"name":this.name,
-			"desc":this.desc,
-			"url":this.url
+			"name": this.name,
+			"desc": this.desc,
+			"url": this.url
 		};
 	}
 }
 
 class BoxManager {
-	constructor(list,default1,is_sync_enabled) {
-		this.#assign_all(list,default1,is_sync_enabled);
+	constructor(list, default1, is_sync_enabled) {
+		this.#assign_all(list, default1, is_sync_enabled);
 	}
 
-	#assign_all(list,default1,is_sync_enabled) {
+	change_default1(name) {
+		let _res = this.list.filter(e=>e.name===name);
+		if (_res.length >= 0) this.default1 = _res[0];
+		else console.log(`We could not find any corresponding Box 4 ${name}!..`);
+	}
+
+	#assign_all(list, default1, is_sync_enabled) {
 		this.list = list;
 		this.default1 = default1;
 		this.is_sync_enabled = is_sync_enabled; // Sync is enabled if it is set to true
@@ -62,90 +66,237 @@ class BoxManager {
 
 	#data() {
 		return {
-			"smartbox_sites":this.list,
-			"smartbox_default":this.default1,
-			"smartbox_is_sync_enabled":this.is_sync_enabled
+			"smartbox_sites": this.list,
+			"smartbox_default": this.default1,
+			"smartbox_is_sync_enabled": this.is_sync_enabled
 		};
-	} 
+	}
 
 	#saveLocal() {
 		chrome.storage.local.set(this.#data(), () => {
-			info("Saved locally!...",this.list,this.default1,this.is_sync_enabled); // Print what data saved & how...
+			console.log("Saved locally!...", this.list, this.default1, this.is_sync_enabled); // Print what data saved & how...
 		});
 	}
 
 	#loadLocal() {
-		chrome.storage.local.get(['smartbox_sites','smartbox_default','smartbox_is_sync_enabled'], (res) => {
-			info("I got locally",res.smartbox_sites,res.smartbox_default,res.smartbox_is_sync_enabled);
-			this.#assign_all(res.smartbox_sites,res.smartbox_default,res.smartbox_is_sync_enabled);
+		chrome.storage.local.get(['smartbox_sites', 'smartbox_default', 'smartbox_is_sync_enabled'], (res) => {
+			console.log("I got locally", res.smartbox_sites, res.smartbox_default, res.smartbox_is_sync_enabled);
+			this.#assign_all(res.smartbox_sites, res.smartbox_default, res.smartbox_is_sync_enabled);
 		});
 	}
 
 	#saveSync() {
 		chrome.storage.sync.set(this.#data(), () => {
-			info("Saved into sync chain!..");
+			console.log("Saved into sync chain!..");
 		});
 	}
 
 	#loadSync() {
-		chrome.storage.sync.get(['smartbox_sites','smartbox_default','smartbox_is_sync_enabled'], (res) => {
-			info("I got locally",res.smartbox_sites,res.smartbox_default,res.smartbox_is_sync_enabled);
-			this.#assign_all(res.smartbox_sites,res.smartbox_default,res.smartbox_is_sync_enabled);
+		chrome.storage.sync.get(['smartbox_sites', 'smartbox_default', 'smartbox_is_sync_enabled'], (res) => {
+			console.log("I got locally", res.smartbox_sites, res.smartbox_default, res.smartbox_is_sync_enabled);
+			this.#assign_all(res.smartbox_sites, res.smartbox_default, res.smartbox_is_sync_enabled);
 		});
 	}
 
 	save() {
-		info(this.is_sync_enabled);
-		if (this.is_sync_enabled) {
-			this.#saveSync();
-		}
+		if (this.is_sync_enabled) this.#saveSync();
 		else this.#saveLocal();
 	}
 
 	load() {
-		if (this.is_sync_enabled) {
-			this.#loadSync();
-		}
+		if (this.is_sync_enabled) this.#loadSync();
 		else this.#loadLocal();
+	}
+
+	add_entry(box) {
+		console.log("box",box);
+		this.list.push(box);
+	}
+
+	add_entryJSON(data) {
+		this.list.push(new Box().fromJSON(data))
+	}
+
+	remove_entry(name) {
+		let _res = this.list.filter(e => e.name === name);
+		console.log("remove",name,_res)
+		if (_res.length >= 0) this.list = this.list.filter(e => e.name != name);
+		else console.log(`Could not find any corresponding Box 4 ${name}!..`);
+	}
+
+	filter(name) {
+		let _res = this.list.filter(e => e.name === name);
+		if (_res.length >= 1) return _res[0];
+		else {
+			console.log(`Could not find any corresponding Box 4 ${name}!..`);
+			return 404;
+		}
 	}
 }
 
 class App {
 	constructor(name) {
-		this.name=name;
+		this.name = name;
 	}
 
 	#boxman
 
+	/***  RegEx Patterns ***/
+	#add_regex = /^(\+<=)[\s\t\v]*(\[.*\])/;
+	#imp_regex = /^(<=)[\s\t\v]*(\[.*\])/;
+	#single_add_regex = /^(\+)([\w\d]+)[\s\t\v]+["']?([\w\d\s]+)["']?[\s\t\v]+(.*)/;
+	#single_rm_regex = /^(\-)([\w\d]+)/;
+	#default1_regex = /^(\!)([\w\d]+)/;
+	#search_regex = /^([\w\d]+)[\s\t\v]+(.*)/;
+
 	#first_install() {
-		info("First Install");
-		this.#boxman.save(sites);
+		console.log("First Install");
+		this.#boxman.save();
+		this.#boxman.load();
 		//save the sites to chrome local/sync storage
 	}
 
-	initialize(sites,default1,is_sync_enabled=false) {
-		this.#boxman = new BoxManager(sites,default1,is_sync_enabled);
-		info(chrome.i18n.getUILanguage());
-		chrome.runtime.onInstalled.addListener((details)=>{
+	#on_install() {
+		chrome.runtime.onInstalled.addListener((details) => {
 			if (details.reason === "install") {
 				this.#first_install();
 			}
 		});
 	}
+
+	#createURL(site,query){
+		let _res = this.#boxman.filter(site);
+		console.log(_res);
+		return _res.url.replace("{%query%}",query);
+	}
+
+	#add_entries(text) {
+		let _res = text.match(this.#add_regex);
+		if ( _res.length == 3 ) {
+			let _sites = JSON.parse(_res[2]);
+			_sites.forEach(e => this.#boxman.add_entryJSON(e));
+		}
+	}
+
+	#add_entry(text) {
+		let _res = text.match(this.#single_add_regex);
+		console.log("add",_res);
+		if ( _res.length == 5 ) {
+			let box = new Box(_res[2],_res[3],_res[4]);
+			this.#boxman.add_entry(box);
+		}
+	}
+
+	#remove_entry(text) {
+		let _res = text.match(this.#single_rm_regex);
+		console.log("remove",_res)
+		if (_res.length == 3) {
+			this.#boxman.remove_entry(_res[2]);
+		}
+	}
+
+	#import_entries(text) {
+		let _res = text.match(this.#imp_regex);
+		if ( _res.length == 3 ) {
+			let sites = JSON.parse(_res[2]);
+			let list = [];
+			if (sites.length >= 0) {
+				sites.forEach(e => list.push(new Box(e.name,e.desc,e.url)));
+			}
+		}
+	}
+
+	#set_default1(text) {
+		let _res = text.match(this.#default1_regex);
+		if (_res.length == 3) this.#boxman.change_default1(_res[2]);
+	}
+
+	#navigate(url) {
+		chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+			chrome.tabs.update(tabs[0].id, {url:url});
+		});
+	}
+
+	#search(text) {
+		let _res = text.match(this.#search_regex)
+		let filter_res = this.#boxman.filter(_res[1])
+		if (_res.length == 3) {
+			console.log(typeof(filter_res))
+			if (filter_res instanceof Box || filter_res instanceof Object) {
+				let site = _res[1]
+				let query = _res[2]
+				let _url = this.#createURL(site,query);
+				this.#navigate(_url)
+			}
+			else this.#searchOnDefaultEngine(text);
+		}
+	}
+
+	#searchOnDefaultEngine(text) {
+		this.#navigate(this.#boxman.default1.url.replace("{%query%}",text));
+	}
+
+	#onInputEntered() {
+		chrome.omnibox.onInputEntered.addListener((text)=>{
+			text=text.trim();
+			console.log(text);
+			if (this.#add_regex.test(text)) {
+				console.log("It's +<=...")
+				this.#add_entries(text);
+			}
+			else if (this.#imp_regex.test(text)) {
+				console.log("It's <=...")
+				this.#import_entries(text);
+			}
+			else if (this.#single_add_regex.test(text)) {
+				console.log("It's +...")
+				this.#add_entry(text);
+			}
+			else if (this.#single_rm_regex.test(text)) {
+				console.log("It's -...")
+				this.#remove_entry(text);
+			}
+			else if (this.#default1_regex.test(text)) {
+				console.log("It's !...")
+				this.#set_default1(text);
+			}
+			else if (this.#search_regex.test(text)) {
+				console.log("It's search...")
+				this.#search(text);
+			}
+			else this.#searchOnDefaultEngine(text);
+			this.#boxman.save();
+		});
+	}
+
+	initialize(sites, default1, is_sync_enabled = false) {
+		this.#boxman = new BoxManager(sites, default1, is_sync_enabled);
+		this.#on_install();
+		this.#onInputEntered();
+		this.#boxman.load();
+	}
 }
+
+//TODO Export Import
+//TODO Toggle Sync
+//TODO Change Default Engine
+//TODO List all
+//TODO List some of them
+//TODO onInputChanged and Started... suggestions...
 
 const app_name = "smartbox";
 var app = new App(app_name);
 var sites = [
-	new Box("ytb","YouTube","https://www.youtube.com/results?search_query={%query%}"),
-	new Box("ggl","Google","https://www.google.com/search?q={%query%}"),
-	new Box("bng","Bing","https://www.bing.com/search?q={%query%}"),
-	new Box("yaho","Yahoo","https://search.yahoo.com/?q={%query%}"),
-	new Box("dckg","DuckDuckGo","https://duckduckgo.com/?q={%query%}"),
-	new Box("iconf","IconFinder","https://www.iconfinder.com/search/?q={%query%}"),
-	new Box("rddt","Reddit","https://www.reddit.com/search/?q={%query%}"),
-	new Box("gthb","GitHub","https://github.com/search?q={%query%}"),
-	new Box("adox","Android Developers","https://developer.android.com/s/results?q={%query%}"),
+	new Box("ytb", "YouTube", "https://www.youtube.com/results?search_query={%query%}"),
+	new Box("ggl", "Google", "https://www.google.com/search?q={%query%}"),
+	new Box("bng", "Bing", "https://www.bing.com/search?q={%query%}"),
+	new Box("yaho", "Yahoo", "https://search.yahoo.com/?q={%query%}"),
+	new Box("dckg", "DuckDuckGo", "https://duckduckgo.com/?q={%query%}"),
+	new Box("iconf", "IconFinder", "https://www.iconfinder.com/search/?q={%query%}"),
+	new Box("rddt", "Reddit", "https://www.reddit.com/search/?q={%query%}"),
+	new Box("gthb", "GitHub", "https://github.com/search?q={%query%}"),
+	new Box("adox", "Android Developers", "https://developer.android.com/s/results?q={%query%}"),
 ];
-var default1=new Box("ggl","Google","https://www.google.com/search?q={%query%}");
+var default1 = sites[0];
 app.initialize(sites, default1);
+
